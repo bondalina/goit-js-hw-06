266169d4aaeae98b252317dfd085f2ff827b6bbc
bondalina <142501520+bondalina@users.noreply.github.com>
2023-11-30 19:48:08 +0200

    update

---
+++ js/task-2.js
@@ -0,0 +1,20 @@
+class Storage {
+  #items;
+  getItems() {
+
+  }
+  addItem(newItem) {
+
+  }
+  removeItem(itemToRemove) {
+    
+  }
+
+}
+
+const storage = new Storage(["Nanitoids", "Prolonger", "Antigravitator"]);
+console.log(storage.getItems()); // ["Nanitoids", "Prolonger", "Antigravitator"]
+storage.addItem("Droid");
+console.log(storage.getItems()); // ["Nanitoids", "Prolonger", "Antigravitator", "Droid"]
+storage.removeItem("Prolonger");
+console.log(storage.getItems()); // ["Nanitoids", "Antigravitator", "Droid"]
